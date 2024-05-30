02c8c6caaa23bad7b07cdab0330334a442d4a5e4
Nurzhigit <Nurjigit00a@gmail.com>
2024-05-30 11:31:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+"use strict";
+
+// a = 25;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//   a: 50,
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// var name = "Ivan";
+// name = "Alex";
+
+// const vehicleBodyWidth = 500;
+// const vehicleBodyLength = 400;
+
+// console.log(
+//   "Shirina kyzoba avto: " + vehicleBodyWidth + ", dlina: " + vehicleBodyLength
+// );
+
+//Request
+
+// let number = 5;
+// console.log(number);
+
+// const person = "Alex";
+// console.log(person);
+
+// const bool = false;
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "John",
+//   age: 23,
+//   isMarried: false,
+// };
+
+// console.log(obj.age);
+
+// let arr = ["plum.png", "orange.jpg", 8];
+// console.log(arr[1]);
+
+// Raznica mezhdy obekti and massivi
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 300,
+// };
+
+// const arr = [1, 2, 4];
+
+// arr[10] = "3435";
+// console.log(arr[10]);
+// console.log(arr);
+
+// console.log(arr[1]);
+
+// const arrObj = {
+//   a: 1,
+//   1: 2,
+//   2: 4,
+//   abc: {
+//     df: [ {}, {}]
+//     def: {
+
+//     }
+//   }
+// };
+
+// const b = "b";
+
+// arrObj[b] = "1243";
+// console.log(arrObj.b);
+
+// console.log(arrObj.a);
+
+// const storeName = "Klars";
+
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ["Billy", "Shelly", "Molly"],
+//   products: {
+//     sneakers: 5000,
+//     shirts: 10000,
+//   },
+//   open: true,
+// };
+
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Do you have a 18?", "18");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your surname?", "");
+// answers[2] = prompt("How old are you?", "18");
+
+// document.write(answers);
+
+// const category = "toys";
+// // console.log("https://someurl.com/" + category + "/" + 4);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Nurzhigit";
+// alert(`Privet, ${user}`);
+
+// let incr = 10;
+// let decr = 10;
+
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked || isClose);
